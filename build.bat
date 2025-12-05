0a4fb7f7970ff34ab0d25032dc58be01904ad600
pete <jordanlahaie@gmail.com>
2025-12-05 10:17:51 -0600

    edit build script

--- build.bat
+++ build.bat
@@ -1,10 +1,5 @@
+REM this doesn't work yet
 pushd build\win\bin
-REM cl /Fepractice /EHsc /Zi /DEBUG /MDd user32.lib gdi32.lib shell32.lib ../../../src/main.c -IX:/repos/glad/include -IX:/repos/SDL/include /link /libpath:../../../lib/win SDL3.lib /NODEFAULTLIB:libcmt
-clang -g -o practice.exe ../../../src/main.c -I../../../../glad/include -I../../../../SDL/include -lSDL3 -lm
-
+clang -g -o practice.exe ../../../src/main.c -I../../../../glad/include -I../../../../SDL/include -lSDL3 
 popd
 
-REM pushd build/lin/bin
-REM clang -g -o practice.bin ../../../src/main.c -I../../../../glad/include -I../../../../SDL/include -lSDL3 -lm
-REM
-REM popd
--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 pushd build/lin/bin
 clang -g -o practice.bin ../../../src/main.c -I../../../../glad/include -I../../../../SDL/include -I../../../../openal-soft/include -I../../../inc -L../../../lib/lin -lopenal -lSDL3 -lm
-
 popd
