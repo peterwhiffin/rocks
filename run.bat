1b6664d44445f57e8be6943a822a8bb371eaa6f8
pete <jordanlahaie@gmail.com>
2025-12-05 10:14:36 -0600

    stopped tracking run scripts

--- run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-pushd build\win\bin
-start x:/rad/raddbg.exe --quit_after_success --auto_run practice.exe
-popd
--- run.sh
+++
@@ -1,3 +0,0 @@
-pushd build/lin/bin 
-gdb -q -ex "set debuginfod enabled off" practice.bin -ex=run -ex=quit
-popd
